2b1bba61578fbf6d6ee46be01870ac5f399dffe3
udoyen <datameshprojects@gmail.com>
2021-04-04 21:43:45 +0100

    Done single document query

--- getNote/__init__.py
+++ getNote/__init__.py
@@ -10,6 +10,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     doc_id = req.params.get('id')
+    #TODO Remove
+    print(f"Doc_id: {doc_id}")
     if not doc_id:
         try:
             req_body = req.get_json()
@@ -20,17 +22,23 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if doc_id:
             try:
-                url = os.environ['jacklineuduak_DOCUMENTDB']
+                url = os.environ['connection']
                 client = pymongo.MongoClient(url)
                 database = client['lab2db']
                 collection = database['notes']
 
-                result = collection.find({id: doc_id})
-                result = dumps(result)
+                result = collection.find_one({"id": int(doc_id)})
+                print(f"Result: {result}") #TODO remove
+                
+                if result is not None: #TODO Add try block to handle null results
+                    result = dumps(result)
+                    return func.HttpResponse(result, mimetype="application/json", charset='utf-8', status_code=200)
+                else:
+                    return func.HttpResponse("No such record exists!", status_code=404)
 
-                return func.HttpResponse(result, mimetype="application/json", charset='utf-8', status_code=200)
-            except:
-                return func.HttpResponse("Bad request,", status_code=400)
+
+            except Exception as e:
+                return func.HttpResponse(f"Bad request: {e}", status_code=400)
             else:
                 return func.HttpResponse(
                     "This HTTP triggered function executed successfully. Pass the id of the document to get back a json document object.",
